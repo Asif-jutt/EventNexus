438177455b31e8350718010be92c6d4d8575b1d5
Asif Hussain <asifhussain5115@gmail.com>
2026-04-15 12:59:34 +0500

    Add project files.

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EventNexus.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/LoginViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel;
+
+namespace EventNexus.Models
+{
+    public class LoginViewModel : INotifyPropertyChanged
+    {
+        private string email;
+        private string password;
+
+        public string Email
+        {
+            get { return email; }
+            set
+            {
+                email = value;
+                OnPropertyChanged(nameof(Email));
+            }
+        }
+
+        public string Password
+        {
+            get { return password; }
+            set
+            {
+                password = value;
+                OnPropertyChanged(nameof(Password));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
