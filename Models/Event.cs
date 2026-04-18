732124b28c9a270eaafab8e8ed96d222c7491f07
Asif Hussain <asifhussain5115@gmail.com>
2026-04-18 19:54:17 +0500

    Update from team code event create

--- Models/Event.cs
+++ Models/Event.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace EventNexus.Models;
 
@@ -7,14 +8,23 @@ public partial class Event
 {
     public int EventId { get; set; }
 
+    [Required(ErrorMessage = "Title is required")]
+    [StringLength(100, MinimumLength = 3, ErrorMessage = "Title must be between 3 and 100 characters")]
     public string? Title { get; set; }
 
+    [Required(ErrorMessage = "Description is required")]
+    [StringLength(500, ErrorMessage = "Description cannot exceed 500 characters")]
     public string? Description { get; set; }
 
+    [Required(ErrorMessage = "Event date is required")]
+    [DataType(DataType.DateTime)]
     public DateTime? EventDate { get; set; }
 
+    [Required(ErrorMessage = "Venue is required")]
     public int? VenueId { get; set; }
 
+    [Required(ErrorMessage = "Status is required")]
+    [RegularExpression("^(Active|Draft|Cancelled)$", ErrorMessage = "Invalid status")]
     public string? Status { get; set; }
 
     public virtual Venue? Venue { get; set; }
--- Models/Venue.cs
+++ Models/Venue.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace EventNexus.Models;
 
@@ -7,10 +8,16 @@ public partial class Venue
 {
     public int VenueId { get; set; }
 
+    [Required(ErrorMessage = "Venue name is required")]
+    [StringLength(100, MinimumLength = 3, ErrorMessage = "Venue name must be between 3 and 100 characters")]
     public string? Name { get; set; }
 
+    [Required(ErrorMessage = "Location is required")]
+    [StringLength(200, ErrorMessage = "Location cannot exceed 200 characters")]
     public string? Location { get; set; }
 
+    [Required(ErrorMessage = "Capacity is required")]
+    [Range(1, 100000, ErrorMessage = "Capacity must be greater than 0")]
     public int? Capacity { get; set; }
 
     public virtual ICollection<Event> Events { get; set; } = new List<Event>();
